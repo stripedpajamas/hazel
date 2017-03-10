8c4b82686b674adec2d6e5a64b8b59de218a206a
peter squicciarini <peter.j.squicciarini@gmail.com>
2017-03-10 15:36:24 -0500

    added some reconnect logic

--- index.js
+++ index.js
@@ -27,15 +27,35 @@ const controller = Botkit.slackbot({
   debug: false,
 });
 
-controller.spawn({
+const hazel = controller.spawn({
   token: connectionInfo.slackToken,
-}).startRTM((err) => {
-  if (err) {
-    winston.log('error', 'Hazel could not connect to Slack. Sorry.');
-  } else {
-    winston.log('info', 'Hazel is connected to Slack!');
-  }
 });
+
+let startCount = 0;
+
+function startHazel() {
+  if (startCount >= 10) {
+    winston.log('error', 'Something went wrong. Hazel tried 10 times to connect to Slack without success.');
+    process.exit();
+  }
+  hazel.startRTM((err) => {
+    if (err) {
+      winston.log('warning', 'Hazel could not connect to Slack. Retrying in 10 seconds...');
+      setTimeout(() => {
+        winston.log('info', '10 seconds are up. Retrying...');
+        startCount += 1;
+        winston.log('warning', `Have tried getting Hazel up and going ${startCount} times now.`);
+        startHazel();
+      }, 10000);
+    } else {
+      winston.log('info', 'Hazel is connected to Slack!');
+      startCount = 0;
+    }
+  });
+}
+
+startHazel();
+
 
 controller.hears(['ticket ([0-9]+)'], ['ambient'], (bot, message) => {
   winston.log('info', 'Hazel heard a ticket # pattern. Sending reply...');
@@ -45,4 +65,10 @@ controller.hears(['ticket ([0-9]+)'], ['ambient'], (bot, message) => {
 controller.hears(['quote', 'proverb', 'wisdom'], ['direct_mention'], (bot, message) => {
   winston.log('info', 'Hazel heard someone mention her directly. Sending reply...');
   bot.reply(message, quotes.getRandomQuote());
+});
+
+controller.on('rtm_close', () => {
+  winston.log('warning', 'Hazel somehow lost connection to Slack. Going to try to connect again.');
+  startCount += 1;
+  startHazel();
 });

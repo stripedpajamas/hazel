a8bc56175db93f9512d5d2a32d9a22475df07034
peter squicciarini <peter.j.squicciarini@gmail.com>
2017-04-18 09:56:41 -0400

    added debug for emg stuff

--- index.js
+++ index.js
@@ -96,6 +96,10 @@ controller.on('rtm_close', (bot) => {
 
 
 // *** Begin Ticket Info/Link Handler *** //
+
+controller.on('message_received', (bot, message) => {
+  winston.log('info', message);
+});
 controller.hears(['New emergency VM:'], ['ambient'], (bot, message) => {
   winston.log('info', 'Heard some emergency VM stuff. Confirming what channel she is in.');
   bot.api.channels.info({ channel: message.channel }, (err, info) => {
